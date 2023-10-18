2bc72306a2c5fad344c900dd50632d61c745c6a8
root <kerian@kerian.myguest.virtualbox.org>
2023-10-18 19:58:08 +0200

    bon deplacement

--- PROECT class_player class_vessel class_input/main.cpp
+++ PROECT class_player class_vessel class_input/main.cpp
@@ -23,6 +23,9 @@ int main()
     /// Initialisation des comptes des players.
     player player_1(1);
     player player_2(2);
+    Vector2f size_base = Vector2f(300.f,450.f);
+    Vector2f size_ligne_fight = Vector2f(1000.f,5.f);
+    Vector2f size_vessel = Vector2f(30.f,30.f);
 
 
                     /// Initialisation MAP
@@ -30,15 +33,15 @@ int main()
         /// MAP BACKGROUND (STATIC)
 
     // CAMPS PLAYERS
-    RectangleShape bloc_base_player(Vector2f(300.f,450.f));
-    RectangleShape bloc_base_AI(Vector2f(300.f,450.f));
+    RectangleShape bloc_base_player(size_base);
+    RectangleShape bloc_base_AI(size_base);
 
     // CHAMP DE BATAILLE
-    RectangleShape bloc_ligne_fight_1(Vector2f(1000.f,2.f));
-    RectangleShape bloc_ligne_fight_2(Vector2f(1000.f,2.f));
-    RectangleShape bloc_ligne_fight_3(Vector2f(1000.f,2.f));
-    RectangleShape bloc_ligne_fight_4(Vector2f(1000.f,2.f));
-    RectangleShape bloc_ligne_fight_5(Vector2f(1000.f,2.f));
+    RectangleShape bloc_ligne_fight_1(size_ligne_fight);
+    RectangleShape bloc_ligne_fight_2(size_ligne_fight);
+    RectangleShape bloc_ligne_fight_3(size_ligne_fight);
+    RectangleShape bloc_ligne_fight_4(size_ligne_fight);
+    RectangleShape bloc_ligne_fight_5(size_ligne_fight);
 
     // MISE EN FORME
     bloc_base_player.setPosition(Vector2f(0.f,50.f));
@@ -66,23 +69,23 @@ int main()
     std::vector<RectangleShape> initsquares;
 
     // Initilisation des carrées
-    RectangleShape redSquare(Vector2f(30, 30));
+    RectangleShape redSquare(size_vessel);
     redSquare.setFillColor(Color::Red);
-    redSquare.setPosition(20, 20);
+    redSquare.setPosition(20, 10);
     initsquares.push_back(redSquare);
 
-    sf::RectangleShape greenSquare(sf::Vector2f(30, 30));
+    sf::RectangleShape greenSquare(size_vessel);
     greenSquare.setFillColor(sf::Color::Green);
-    greenSquare.setPosition(80, 20);
+    greenSquare.setPosition(80, 10);
     initsquares.push_back(greenSquare);
 
-    sf::RectangleShape blueSquare(sf::Vector2f(30, 30));
+    sf::RectangleShape blueSquare(size_vessel);
     blueSquare.setFillColor(sf::Color::Blue);
-    blueSquare.setPosition(140, 20);
+    blueSquare.setPosition(140, 10);
     initsquares.push_back(blueSquare);
 
 
-    sf::RectangleShape followingSquare(sf::Vector2f(30, 30));
+    RectangleShape followingSquare(size_vessel);
     followingSquare.setFillColor(sf::Color::Red);
 
                 /// FIN INITIALISATION MAP
@@ -91,7 +94,7 @@ int main()
 
     Input input; // Initialisation Touches
 
-    bool isFollowing = false;
+    bool vessel_select = false;
     int squaresi = -1;
 
 
@@ -112,51 +115,61 @@ int main()
                 sf::FloatRect redSquareBounds = redSquare.getGlobalBounds();
                 sf::FloatRect blueSquareBounds = blueSquare.getGlobalBounds();
                 sf::FloatRect greenSquareBounds = greenSquare.getGlobalBounds();
-                    if (redSquareBounds.contains(static_cast<float>(mousePosition.x), static_cast<float>(mousePosition.y)))
+
+
+                if (redSquareBounds.contains(static_cast<float>(mousePosition.x), static_cast<float>(mousePosition.y)))
+                {
+                    if (vessel_select == false) {vessel_select=true;}
+
+                    sf::RectangleShape newSquare;
+                    newSquare.setSize(size_vessel);
+                    newSquare.setFillColor(sf::Color::Red);
+                    newSquare.setPosition(30, 30);
+                    squares.push_back(newSquare);
+                    squaresi += 1;
+
+                }
+
+                else if (blueSquareBounds.contains(static_cast<float>(mousePosition.x), static_cast<float>(mousePosition.y)))
+                {
+                    if (vessel_select == false) {vessel_select=true;}
+
+                    sf::RectangleShape newSquare;
+                    newSquare.setSize(size_vessel);
+                    newSquare.setFillColor(sf::Color::Blue);
+                    newSquare.setPosition(80, 20);
+                    squares.push_back(newSquare);
+                    squaresi += 1;
+                }
+
+                else if (greenSquareBounds.contains(static_cast<float>(mousePosition.x), static_cast<float>(mousePosition.y)))
+                {
+                    if (vessel_select == false) {vessel_select=true;}
+
+                    sf::RectangleShape newSquare;
+                    newSquare.setSize(size_vessel);
+                    newSquare.setFillColor(sf::Color::Green);
+                    newSquare.setPosition(140, 20);
+                    squares.push_back(newSquare);
+                    squaresi += 1;
+                }
+
+                else if (vessel_select == true)
+                {
+                    FloatRect rect_vessel = squares[squaresi].getGlobalBounds();
+                    if (rect_vessel.intersects(bloc_ligne_fight_1.getGlobalBounds()));
                     {
-
-                        sf::RectangleShape newSquare;
-                        newSquare.setSize(sf::Vector2f(30, 30));
-                        newSquare.setFillColor(sf::Color::Red);
-                        newSquare.setPosition(30, 30);
-                        squares.push_back(newSquare);
-                        squaresi += 1;
-                        isFollowing = true;
+                        vessel_select == false; /// Probleme pas de décrochage...
+                        cout << "bien placé !" << endl;
                     }
+                }
 
-                    else if (blueSquareBounds.contains(static_cast<float>(mousePosition.x), static_cast<float>(mousePosition.y)))
-                    {
 
-                        sf::RectangleShape newSquare;
-                        newSquare.setSize(sf::Vector2f(30, 30));
-                        newSquare.setFillColor(sf::Color::Blue);
-                        newSquare.setPosition(80, 20);
-                        squares.push_back(newSquare);
-                        squaresi += 1;
-                        isFollowing = true;
-                    }
-
-                    else if (greenSquareBounds.contains(static_cast<float>(mousePosition.x), static_cast<float>(mousePosition.y)))
-                    {
-
-                        sf::RectangleShape newSquare;
-                        newSquare.setSize(sf::Vector2f(30, 30));
-                        newSquare.setFillColor(sf::Color::Green);
-                        newSquare.setPosition(140, 20);
-                        squares.push_back(newSquare);
-                        squaresi += 1;
-                        isFollowing = true;
-                    }
             }
 //-----------------------------------------------------------------------------------
             if (souris.left == false)
             {
-                    if (isFollowing)
-                    {
-                        isFollowing = false;
-                        sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
-                        squares[squaresi].setPosition(static_cast<float>(mousePosition.x), static_cast<float>(mousePosition.y));
-                    }
+
             }
 
         } // FIN POLL_EVENT
@@ -167,11 +180,14 @@ int main()
 
         window.draw(bloc_base_player);
         window.draw(bloc_base_AI);
+
+        /*
         window.draw(bloc_ligne_fight_1);
         window.draw(bloc_ligne_fight_2);
         window.draw(bloc_ligne_fight_3);
         window.draw(bloc_ligne_fight_4);
         window.draw(bloc_ligne_fight_5);
+        */
 
         /// BACKGROUND
 
@@ -186,7 +202,7 @@ int main()
         }
 
 
-        if (isFollowing)
+        if (vessel_select)
         {
             sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
             squares[squaresi].setPosition(static_cast<float>(mousePosition.x), static_cast<float>(mousePosition.y));
