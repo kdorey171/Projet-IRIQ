abfd627cf95dbc0f5279e9fdf218680f12bf4ed1
JulienDesdo <147617422+JulienDesdo@users.noreply.github.com>
2023-11-18 21:40:56 +0100

    Add files via upload
    "Modifications Textutres + Ajout class_tower à completer pour faire direct new_tower lors de la création d'un batiment + refaire une methode de vessel visant à appliquer des textures : ce sera utiles pour changer les textures lorsqu'un batiment va upgrade de niveau + quelques des trucs on bougés et vont être encore arrangés surtout au niveau du main; peut être une classe pour les sprites?"

---
+++ Projet-IRIQ modif_julien/PROECT class_player class_vessel class_input/include/class_vessel.h
@@ -0,0 +1,85 @@
+#ifndef CLASS_VESSEL_H
+#define CLASS_VESSEL_H
+
+#include "player.h"
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class vessel {
+protected :
+    int num_player;
+    int PV;
+    int lvl;
+    int prix;
+    int type;
+    sf::RectangleShape position_vessel;
+
+public :
+    //Affichage
+    sf::RectangleShape afficher();
+
+    //constructor
+    vessel();
+    // Desturctor
+    virtual ~vessel();
+
+    vessel& operator=(const vessel&);
+
+    //methods
+    void set_PV(int pv);
+    int get_PV();
+
+    void set_lvl(int new_level);
+    int get_lvl();
+
+    int get_type();
+    int get_player(){return num_player;}
+
+    int get_prix();
+
+
+    sf::Vector2f get_position();
+    void set_position(sf::Vector2f);
+    void set_size(sf::Vector2f);
+    void set_color(sf::Color);
+    void set_texture(sf::Texture& texture); /// NE MARCHE PAS, A FAIRE, utile quand les batiment devrons monter de niveau.
+    Color get_color();
+
+    bool allow_build(player);
+
+
+    /// fonction classe fille en virtual
+
+
+    virtual bool get_spawn_possible(){return false;}
+    virtual void set_spawn(bool){}
+    virtual float get_clockspawn(){return 0.f;}
+    virtual void reset_clockspawn(){}
+    virtual int get_nbunite(){return 0;}
+    virtual void set_nbunite(int){}
+
+
+    virtual void set_gain(int){}
+    virtual int get_gain(){return 0;}
+    virtual float get_clock(){return 0.f;}
+    virtual void reset_clock(){}
+    virtual void set_gain_time(int){}
+    virtual int get_gain_time(){return 0;}
+
+
+    /*
+    build(); // construire un bâtiment, class bank allow?
+    create_unit() // class unit
+    */
+
+
+
+};
+
+#endif
---
+++ Projet-IRIQ modif_julien/README.md
@@ -0,0 +1,14 @@
+# Projet-IRIQ
+Jeu
+
+Kerian Key:
+
+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
+
+Bastien key:
+
+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
+
+Julien key:
+
+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
